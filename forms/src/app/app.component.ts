3f75b07ecea59280baeb4db8712b21e95bf74eed
Shamim <innocentb758@gmail.com>
2018-12-19 17:14:52 +0600

    Form with error handle and validation

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from './model/user';
 import { Address } from './model/address';
+import { EnrollmentService } from './service/enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,27 @@ import { Address } from './model/address';
 })
 export class AppComponent {
   title = 'Angular Forms';
+  languageHasError = true;
+  errorMsg = "";
+
   public languages = ["Java", "Angular", "Ruby", "C++"];
 
-  userModel = new User('', 'kamrul@mail.com', 65465432132, '', 'morning', true, new Address("", "",""));
+  userModel = new User('Kamrul', 'kamrul@mail.com', 65465432132, 'default', 'morning', true, new Address("", "",""));
+
+  constructor(private enrollmentService:EnrollmentService) {}
+
+  validateLanguage(value:string) {
+    if(value === "default") {
+      this.languageHasError = true;
+    } else {
+      this.languageHasError = false;
+    }
+  }
+
+  onSubmit() {
+    this.enrollmentService.enroll(this.userModel)
+      .subscribe(data => console.log("Success! ", data),
+      err => this.errorMsg = err.statusText
+      )
+  }
 }
---
+++ forms/src/app/service/enrollment.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { User } from '../model/user';
+import { catchError } from 'rxjs/operators'
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnrollmentService {
+
+  url = "";
+
+  constructor(private http: HttpClient) { }
+
+  enroll(user:User) {
+    return this.http.post<any>(this.url, user).pipe(catchError(this.errorHandler));
+  }
+
+  errorHandler(error:HttpErrorResponse) {
+    return throwError(error);
+  }
+}
